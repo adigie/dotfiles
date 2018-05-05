ddb9b38667c3202e68bda5dbebe46f4afc55bcab
Adrian Gielniewski <adrian.gielniewski@gmail.com>
2018-05-05 21:52:56 +0200

    Add feh service.

--- link.sh
+++ link.sh
@@ -11,3 +11,16 @@ ln -s $ROOT_DIR/.config/i3 ~/.config/i3
 rm ~/.config/i3status
 ln -s $ROOT_DIR/.config/i3status ~/.config/i3status
 
+rm ~/.fehbg
+ln -s $ROOT_DIR/.fehbg ~/.fehbg
+
+rm ~/.config/systemd/user/feh.timer
+ln -s $ROOT_DIR/.config/systemd/user/feh.timer ~/.config/systemd/user/feh.timer
+
+rm ~/.config/systemd/user/feh.service
+ln -s $ROOT_DIR/.config/systemd/user/feh.service ~/.config/systemd/user/feh.service
+
+systemctl --user daemon-reload
+systemctl --user enable feh.timer
+systemctl --user start feh.timer
+
