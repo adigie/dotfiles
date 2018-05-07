fd13b4f860aadd9d3495f1685d3d216dc2734271
Adrian Gielniewski <adrian.gielniewski@gmail.com>
2018-05-07 09:51:15 +0200

    cd

--- link.sh
+++ link.sh
@@ -2,25 +2,28 @@
 
 ROOT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-rm ~/.vimrc
+rm -f ~/.vimrc
 ln -s $ROOT_DIR/.vimrc ~/.vimrc
 
-rm ~/.config/i3
+rm -rf ~/.config/i3
 ln -s $ROOT_DIR/.config/i3 ~/.config/i3
 
-rm ~/.config/i3status
+rm -rf ~/.config/i3status
 ln -s $ROOT_DIR/.config/i3status ~/.config/i3status
 
-rm ~/.fehbg
-ln -s $ROOT_DIR/.fehbg ~/.fehbg
+rm -rf ~/.config/dunst
+ln -s $ROOT_DIR/.config/dunst ~/.config/dunst
 
-rm ~/.config/systemd/user/feh.timer
-ln -s $ROOT_DIR/.config/systemd/user/feh.timer ~/.config/systemd/user/feh.timer
-
-rm ~/.config/systemd/user/feh.service
-ln -s $ROOT_DIR/.config/systemd/user/feh.service ~/.config/systemd/user/feh.service
-
-systemctl --user daemon-reload
-systemctl --user enable feh.timer
-systemctl --user start feh.timer
+#rm ~/.fehbg
+#ln -s $ROOT_DIR/.fehbg ~/.fehbg
+#
+#rm ~/.config/systemd/user/feh.timer
+#ln -s $ROOT_DIR/.config/systemd/user/feh.timer ~/.config/systemd/user/feh.timer
+#
+#rm ~/.config/systemd/user/feh.service
+#ln -s $ROOT_DIR/.config/systemd/user/feh.service ~/.config/systemd/user/feh.service
+#
+#systemctl --user daemon-reload
+#systemctl --user enable feh.timer
+#systemctl --user start feh.timer
 
