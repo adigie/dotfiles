e0b77f8f2092f4c879242d1d4fd738bf95924813
Adrian Gielniewski <adrian.gielniewski@gmail.com>
2018-10-11 10:06:12 +0200

    Remove feh config

--- link.sh
+++ link.sh
@@ -14,16 +14,3 @@ ln -s $ROOT_DIR/.config/i3status ~/.config/i3status
 rm -rf ~/.config/dunst
 ln -s $ROOT_DIR/.config/dunst ~/.config/dunst
 
-#rm ~/.fehbg
-#ln -s $ROOT_DIR/.fehbg ~/.fehbg
-#
-#rm ~/.config/systemd/user/feh.timer
-#ln -s $ROOT_DIR/.config/systemd/user/feh.timer ~/.config/systemd/user/feh.timer
-#
-#rm ~/.config/systemd/user/feh.service
-#ln -s $ROOT_DIR/.config/systemd/user/feh.service ~/.config/systemd/user/feh.service
-#
-#systemctl --user daemon-reload
-#systemctl --user enable feh.timer
-#systemctl --user start feh.timer
-
